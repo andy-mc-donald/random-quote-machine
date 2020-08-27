e493a36f976ef179dd9ec7447c644650945bd670
andy <andy.william.mcdonald@gmail.com>
2020-08-27 10:06:26 +0100

    Updates

---
+++ precache-manifest.2052f6a6651489e4345fb10f68e8516d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791b92f34c9077857c01de05276d8fce",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "75aeccdfd908105df4bd",
+    "url": "/random-quote-machine/static/css/main.4efbebd7.chunk.css"
+  },
+  {
+    "revision": "828c704ce711f0964864",
+    "url": "/random-quote-machine/static/js/2.f0dfa6fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.f0dfa6fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75aeccdfd908105df4bd",
+    "url": "/random-quote-machine/static/js/main.b0b36e13.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
